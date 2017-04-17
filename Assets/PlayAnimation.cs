ce9357f41f980626805468842054f24e592483b7
Kaj <kaaiman99@hotmail.com>
2017-04-17 19:55:32 +0200

    Initial commit

---
+++ Assets/PlayAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+	private Animator animator;
+	private double inc;
+	public float speed;
+	AnimatorClipInfo[] animClip;
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	void Update () {
+		if (inc <= 0) {
+			inc = 0;
+		}
+		if(inc >= 5.650f){
+			inc = 5.650f;
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			inc += speed / 100;
+			animator.SetTime (inc);
+		} else {
+			inc -= speed / 100;
+			animator.SetTime (inc);
+		}
+	}
+}
